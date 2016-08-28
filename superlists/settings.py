60bedb4fbed7d5b4db80e85c107eb4f1eb59e38f
AnasMunir <anas.munir.92@gmail.com>
2016-08-29 01:56:48 +0500

    Added postgresql and removed sqlite3'

--- superlists/settings.py
+++ superlists/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'superlists.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../database/db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tdd_project',
+        'USER': 'anasmunir',
+        'PASSWORD': 'tdd_project',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
